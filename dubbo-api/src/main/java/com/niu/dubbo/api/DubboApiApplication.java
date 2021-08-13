d857fb224969b9fb9174b01e19a6c0231e4331de
niusayoko <13522283304@163.com>
2021-08-13 16:01:30 +0800

    init

--- dubbo-api/src/main/java/com/niu/dubbo/api/DubboApiApplication.java
+++ dubbo-api/src/main/java/com/niu/dubbo/api/DubboApiApplication.java
@@ -1,4 +1,4 @@
-package com.niu.dubbo.provider;
+package com.niu.dubbo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
