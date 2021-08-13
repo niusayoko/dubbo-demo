2eab4cae1a6ad483e3f18fa37a9614aebde6572c
niusayoko <13522283304@163.com>
2021-08-13 15:42:32 +0800

    init

---
+++ dubbo-provider/src/main/java/com/niu/dubbo/provider/api/DubboUserApi.java
@@ -0,0 +1,14 @@
+package com.niu.dubbo.provider.api;
+
+public interface DubboUserApi {
+
+    /**
+     * 根据用户id获取用户名dubbo接口
+     *
+     * @param id
+     * @return
+     * @author niuqingsong
+     * @Date 2021/8/13
+     */
+    String getUser(Long id);
+}
---
+++ dubbo-provider/src/test/java/com/niu/dubbo.provider/DubboProviderApplicationTests.java
@@ -0,0 +1,12 @@
+package com.niu.dubbo.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DubboProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
